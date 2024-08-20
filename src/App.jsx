fd6a0941166d6023ea0e0088dfa85c7fb5045a27
daniahih <daniahih310@gmail.com>
2024-08-20 11:53:10 +0300

    fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import "./App.css";
+
 import AllEvent from "./pages/AllEvent/AllEvent";
 import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
+import Layout from "./Components/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Element: <Home />,
+    element: <Layout />, // Use the Layout component here
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "all-events", element: <AllEvent /> },
+      { path: "about", element: <About /> },
+    ],
   },
-  { path: "/all-events", element: <AllEvent /> },
-  { path: "/about", element: <About /> },
 ]);
+
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
 }
